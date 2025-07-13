0d15507f1eaa11741cd82bc2cc3c849c7f5af81e
aleksanr_penkin <sashapenkin12@mail.ru>
2025-07-13 10:18:34 +0600

    fix: delete unused logic for db init

--- db/setup.sh
+++ db/setup.sh
@@ -1,7 +1,3 @@
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-  ALTER SYSTEM SET log_destination TO 'stderr';
-  ALTER SYSTEM SET logging_collector TO 'on';
-  ALTER SYSTEM SET log_directory TO '/var/log/postgresql';
-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB"
